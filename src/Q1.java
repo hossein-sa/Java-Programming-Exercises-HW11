0b16277b0a5d7f7359f9ef4cf5f0b5525fe17a2b
Aref Sa <arefsa6@gmail.com>
2022-08-29 17:20:03 +0430

    Creating Start Menu for Choosing the Questions

---
+++ src/Q1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Q1 {
+    public static void showQ1Menu() {
+        System.out.print("Please enter your word: ");
+        String inputWord = new Scanner(System.in).next();
+        int lengthWord = inputWord.length();
+        permute(inputWord, 0, lengthWord - 1);
+
+    }
+
+    public static void permute(String str, int l, int r) {
+        if (l == r)
+            System.out.println(str);
+        else {
+            for (int i = l; i <= r; i++) {
+                str = swap(str, l, i);
+                permute(str, l + 1, r);
+                str = swap(str, l, i);
+            }
+        }
+    }
+
+    public static String swap(String a, int i, int j) {
+        char temp;
+        char[] charArray = a.toCharArray();
+        temp = charArray[i];
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+        return String.valueOf(charArray);
+    }
+}
