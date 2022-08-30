3e86f72b08e42aee91e35d951783a12a869acf6d
Aref Sa <arefsa6@gmail.com>
2022-08-30 08:55:38 +0430

    Make a HashMap and put results in it for Q1

--- src/Q1.java
+++ src/Q1.java
@@ -1,17 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Q1 {
+    public static Map<Integer, String> permuteMap = new HashMap<>();
+    public static int count = 1;
+
     public static void showQ1Menu() {
         System.out.print("Please enter your word: ");
         String inputWord = new Scanner(System.in).next();
         int lengthWord = inputWord.length();
         permute(inputWord, 0, lengthWord - 1);
+        System.out.println(permuteMap.values());
 
     }
 
     public static void permute(String str, int l, int r) {
-        if (l == r)
-            System.out.println(str);
+        if (l == r){
+            permuteMap.put(count++,str);
+        }
         else {
             for (int i = l; i <= r; i++) {
                 str = swap(str, l, i);
